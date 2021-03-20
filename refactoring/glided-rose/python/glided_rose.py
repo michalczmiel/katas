c8f9b420b93daa787bed89f1d227ebbc86889d47
Michał Czmiel <michalczmiel@outlook.com>
2021-03-20 18:36:50 +0100

    extract legendardy item logic in glided rose kata

--- refactoring/glided-rose/python/glided_rose.py
+++ refactoring/glided-rose/python/glided_rose.py
@@ -1,7 +1,15 @@
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, replace
 
 
-class GildedRose:
+@dataclass
+class Item:
+    name: str
+    sell_in: int
+    quality: int
+
+
+class GildedRoseLegacy:
     def __init__(self, items):
         self.items = items
 
@@ -40,7 +48,59 @@ class GildedRose:
 
 
 @dataclass
-class Item:
-    name: str
-    sell_in: int
-    quality: int
+class LegendaryItem:
+    item: Item
+
+    def update_quality(self):
+        pass
+
+
+@dataclass
+class DefaultItem:
+    item: Item
+
+    def update_quality(self):
+        if (
+            self.item.name != "Aged Brie"
+            and self.item.name != "Backstage passes to a TAFKAL80ETC concert"
+        ):
+            if self.item.quality > 0:
+                self.item.quality = self.item.quality - 1
+        else:
+            if self.item.quality < 50:
+                self.item.quality = self.item.quality + 1
+                if self.item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if self.item.sell_in < 11:
+                        if self.item.quality < 50:
+                            self.item.quality = self.item.quality + 1
+                    if self.item.sell_in < 6:
+                        if self.item.quality < 50:
+                            self.item.quality = self.item.quality + 1
+
+        self.item.sell_in = self.item.sell_in - 1
+
+        if self.item.sell_in < 0:
+            if self.item.name != "Aged Brie":
+                if self.item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if self.item.quality > 0:
+                        self.item.quality = self.item.quality - 1
+                else:
+                    self.item.quality = self.item.quality - self.item.quality
+            else:
+                if self.item.quality < 50:
+                    self.item.quality = self.item.quality + 1
+
+
+class GildedRose:
+    def __init__(self, items):
+        self.items = items
+
+    def _is_legendary_item(self, item: Item) -> bool:
+        return item.name == "Sulfuras, Hand of Ragnaros"
+
+    def update_quality(self):
+        for item in self.items:
+            if self._is_legendary_item(item):
+                LegendaryItem(item).update_quality()
+            else:
+                DefaultItem(item).update_quality()
