708143489bcde8ea743de260652c45bdc0db2f95
Michał Czmiel <michalczmiel@outlook.com>
2021-05-30 20:05:40 +0200

    use pytest fixtures

--- refactoring/theatrical-players/python/tests/test_statement.py
+++ refactoring/theatrical-players/python/tests/test_statement.py
@@ -7,19 +7,35 @@ from approvaltests.utils import get_adjacent_file
 from statement.statement import statement
 
 
-def test_example_statement():
+@pytest.fixture
+def invoice():
     with open(get_adjacent_file("invoice.json")) as f:
-        invoice = json.loads(f.read())
+        return json.loads(f.read())
+
+
+@pytest.fixture
+def plays():
     with open(get_adjacent_file("plays.json")) as f:
-        plays = json.loads(f.read())
+        return json.loads(f.read())
+
+
+@pytest.fixture
+def invoice_new_plays():
+    with open(get_adjacent_file("invoice_new_plays.json")) as f:
+        return json.loads(f.read())
+
+
+@pytest.fixture
+def new_plays():
+    with open(get_adjacent_file("new_plays.json")) as f:
+        return json.loads(f.read())
+
+
+def test_example_statement(invoice, plays):
     verify(statement(invoice, plays))
 
 
-def test_statement_with_new_play_types():
-    with open(get_adjacent_file("invoice_new_plays.json")) as f:
-        invoice = json.loads(f.read())
-    with open(get_adjacent_file("new_plays.json")) as f:
-        plays = json.loads(f.read())
+def test_statement_with_new_play_types(invoice_new_plays, new_plays):
     with pytest.raises(ValueError) as exception_info:
-        statement(invoice, plays)
+        statement(invoice_new_plays, new_plays)
     assert "unknown type" in str(exception_info.value)
