750d09155d3839e005c03203c9e45809030d8d47
Michał Czmiel <michalczmiel@outlook.com>
2021-06-03 14:32:10 +0200

    extract amount calculation logic

--- refactoring/theatrical-players/python/statement/statement.py
+++ refactoring/theatrical-players/python/statement/statement.py
@@ -1,5 +1,7 @@
 import math
+from dataclasses import dataclass
 from typing import TypedDict, List, Dict
+from abc import ABC, abstractmethod
 
 
 class Performance(TypedDict):
@@ -40,6 +42,53 @@ def render_statement(
     return result
 
 
+class CompletedPlay(ABC):
+    @property
+    @abstractmethod
+    def audience(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def amount(self) -> int:
+        pass
+
+
+@dataclass(frozen=True)
+class ComedyCompletedPlay(CompletedPlay):
+    audience: int
+
+    @property
+    def amount(self) -> int:
+        this_amount = 30000
+        if self.audience > 20:
+            this_amount += 10000 + 500 * (self.audience - 20)
+
+        this_amount += 300 * self.audience
+        return this_amount
+
+
+@dataclass(frozen=True)
+class TragedyCompletedPlay(CompletedPlay):
+    audience: int
+
+    @property
+    def amount(self) -> int:
+        this_amount = 40000
+        if self.audience > 30:
+            this_amount += 1000 * (self.audience - 30)
+        return this_amount
+
+
+def map_to_completed_play(play: Play, performance: Performance) -> CompletedPlay:
+    if play["type"] == "tragedy":
+        return TragedyCompletedPlay(audience=performance["audience"])
+    elif play["type"] == "comedy":
+        return ComedyCompletedPlay(audience=performance["audience"])
+    else:
+        raise ValueError(f'unknown type: {play["type"]}')
+
+
 def statement(invoice: Invoice, plays: Dict[str, Play]) -> str:
     total_amount = 0
     volume_credits = 0
@@ -47,19 +96,8 @@ def statement(invoice: Invoice, plays: Dict[str, Play]) -> str:
 
     for perf in invoice["performances"]:
         play = plays[perf["playID"]]
-        if play["type"] == "tragedy":
-            this_amount = 40000
-            if perf["audience"] > 30:
-                this_amount += 1000 * (perf["audience"] - 30)
-        elif play["type"] == "comedy":
-            this_amount = 30000
-            if perf["audience"] > 20:
-                this_amount += 10000 + 500 * (perf["audience"] - 20)
 
-            this_amount += 300 * perf["audience"]
-
-        else:
-            raise ValueError(f'unknown type: {play["type"]}')
+        completed_play = map_to_completed_play(play, perf)
 
         # add volume credits
         volume_credits += max(perf["audience"] - 30, 0)
@@ -70,12 +108,12 @@ def statement(invoice: Invoice, plays: Dict[str, Play]) -> str:
         summaries.append(
             {
                 "name": play["name"],
-                "amount": this_amount,
-                "audience": perf["audience"],
+                "amount": completed_play.amount,
+                "audience": completed_play.audience,
             }
         )
 
-        total_amount += this_amount
+        total_amount += completed_play.amount
 
     result = render_statement(
         invoice["customer"],
