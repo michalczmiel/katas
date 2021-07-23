4823ebdeeb868b3f345dc468e8f25e1ee9a4e333
Michał Czmiel <michalczmiel@outlook.com>
2021-07-23 23:31:19 +0200

    remove unused code

--- devops/flask_container_ci/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SECRET_KEY = "shhh"
-CSRF_ENABLED = True
-
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
