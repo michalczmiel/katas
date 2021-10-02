a0d05627d7520e45a47251ee6c982b0dc7c37651
Michał Czmiel <michalczmiel@outlook.com>
2021-10-02 16:38:12 +0200

    refactor code to extract delimiter detection logic

--- tdd/string-calculator/string_calculator.py
+++ tdd/string-calculator/string_calculator.py
@@ -1,15 +1,20 @@
 import re
+from typing import Optional
 
 class StringCalculator:
+    def _get_custom_delimiter(self, numbers: str) -> Optional[str]:
+        if not numbers.startswith("//"):
+            return
+        custom_delimiter = numbers.split("\n")[0][2]
+        return custom_delimiter
+
     def add(self, numbers: str):
         if not numbers:
             return 0
-        custom_delimiter = None
-        if numbers.startswith("//"):
-            custom_delimiter = numbers.split("\n")[0][2]
-            numbers = numbers[4:]
         delimiters = ",|\n"
+        custom_delimiter = self._get_custom_delimiter(numbers)
         if custom_delimiter:
+            numbers = numbers[4:]
             delimiters += f"|{custom_delimiter}"
         numbers = re.split(delimiters, numbers)
         numbers = [int(number) for number in numbers]
