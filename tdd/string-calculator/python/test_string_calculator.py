14bd1901be17a92614acd1599e9ece891d6ee54b
Michał Czmiel <michalczmiel@outlook.com>
2021-10-02 18:16:31 +0200

    parametrize tests

--- tdd/string-calculator/python/test_string_calculator.py
+++ tdd/string-calculator/python/test_string_calculator.py
@@ -12,24 +12,52 @@ def test_returns_zero_for_empty_string(calculator):
     assert calculator.add("") == 0
 
 
-def test_returns_number_if_one_number_is_given(calculator):
-    assert calculator.add("1") == 1
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+    ],
+)
+def test_returns_number_if_single_number_is_given(calculator, numbers, expected):
+    assert calculator.add(numbers) == expected
 
 
-def test_returns_sum_if_two_numbers_are_given(calculator):
-    assert calculator.add("1,2") == 3
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("0,1", 1),
+        ("1,2,3", 6),
+        ("1,2,3,4", 10),
+    ],
+)
+def test_returns_sum_if_multiple_numbers_are_given(calculator, numbers, expected):
+    assert calculator.add(numbers) == expected
 
 
-def test_returns_sum_if_multiple_numbers_are_given(calculator):
-    assert calculator.add("1,2,3,4") == 10
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("1\n2,3", 6),
+        ("1\n2\n3", 6),
+        ("1,2\n3,4", 10),
+    ],
+)
+def test_returns_sum_if_delimiter_is_also_new_line(calculator, numbers, expected):
+    assert calculator.add(numbers) == expected
 
 
-def test_returns_sum_if_delimiter_is_also_new_line(calculator):
-    assert calculator.add("1\n2,3") == 6
-
-
-def test_returns_sum_if_delimiter_is_changed(calculator):
-    assert calculator.add("//;\n1;2") == 3
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("//;\n1;2;3", 6),
+        ("//:\n1:2:3", 6),
+        ("///\n1/2/3", 6),
+    ],
+)
+def test_returns_sum_if_delimiter_is_changed(calculator, numbers, expected):
+    assert calculator.add(numbers) == expected
 
 
 def test_throws_error_when_negative_number_is_given(calculator):
@@ -44,18 +72,37 @@ def test_throws_error_when_multiple_negative_numbers_is_given(calculator):
     assert "negatives not allowed, but found -1, -2" in str(exception_info.value)
 
 
-def test_ignores_numbers_bigger_than_1000(calculator):
-    assert calculator.add("2,1000") == 1002
-    assert calculator.add("2,1001") == 2
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("2,1000", 1002),
+        ("2,1001", 2),
+        ("2,1001,1002", 2),
+    ],
+)
+def test_ignores_big_numbers(calculator, numbers, expected):
+    assert calculator.add(numbers) == expected
 
 
-def test_returns_sum_if_any_length_delimiter_is_changed(calculator):
-    assert calculator.add("//[***]\n1***2***3") == 6
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("//[***]\n1***2***3", 6),
+        ("//[;;]\n1;;2;;3;;4", 10),
+        ("//[%%%%]\n1%%%%2", 3),
+    ],
+)
+def test_returns_sum_if_any_length_delimiter_is_changed(calculator, numbers , expected):
+    assert calculator.add(numbers) == expected
 
 
-def test_returns_sum_if_multiple_delimiters_are_set(calculator):
-    assert calculator.add("//[*][%]\n1*2%3") == 6
-
-
-def test_returns_sum_if_multiple_any_length_delimiters_are_set(calculator):
-    assert calculator.add("//[***][%%]\n1***2%%3") == 6
+@pytest.mark.parametrize(
+    "numbers, expected",
+    [
+        ("//[*][%]\n1*2%3", 6),
+        ("//[*][%][&]\n1*2%3&4", 10),
+        ("//[***][%%]\n1***2%%3", 6),
+    ],
+)
+def test_returns_sum_if_multiple_delimiters_are_set(calculator, numbers, expected):
+    assert calculator.add(numbers) == expected
