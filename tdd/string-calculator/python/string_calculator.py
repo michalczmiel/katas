25453ea870141a68eeeba30cc75a1d1d18cc4df3
Michał Czmiel <michalczmiel@outlook.com>
2021-10-02 17:58:47 +0200

    extract logic to detect single char delimiter

--- tdd/string-calculator/python/string_calculator.py
+++ tdd/string-calculator/python/string_calculator.py
@@ -1,21 +1,24 @@
 import re
-from typing import Iterable, List, Optional, Tuple
+from typing import Iterable, List, Tuple
 
 
 class StringCalculator:
     def __init__(self) -> None:
         self._default_delimiters: Tuple[str] = (",", "\n")
         self._max_big_number: int = 1000
-        self._delimeter_change_chars: str = "//"
+        self._delimiter_change_chars: str = "//"
+
+    def _has_single_char_delimiter(self, delimiter_part: str) -> bool:
+        return len(self._delimiter_change_chars) + 1 == len(delimiter_part)
 
     def _parse_raw_numbers(self, raw_numbers: str) -> Tuple[str, List[str]]:
-        if not raw_numbers.startswith(self._delimeter_change_chars):
+        if not raw_numbers.startswith(self._delimiter_change_chars):
             return raw_numbers, []
 
         delimiter_part, numbers_part = raw_numbers.split("\n")
 
-        if "[" not in delimiter_part and "]" not in delimiter_part:
-            custom_delimiter = delimiter_part[len(self._delimeter_change_chars) :]
+        if self._has_single_char_delimiter(delimiter_part):
+            custom_delimiter = delimiter_part[len(self._delimiter_change_chars) :]
             return numbers_part, [custom_delimiter]
 
         custom_delimiters = re.findall(r"\[(.*?)\]", delimiter_part)
@@ -48,8 +51,11 @@ class StringCalculator:
     def add(self, raw_numbers: str) -> int:
         if not raw_numbers:
             return 0
+
         numbers, custom_delimiters = self._parse_raw_numbers(raw_numbers)
         numbers = self._split_numbers(numbers, custom_delimiters)
-        numbers = self._ignore_big_numbers(numbers)
+
         self._assert_no_negative_numbers(numbers)
+        numbers = self._ignore_big_numbers(numbers)
+
         return sum(numbers)
