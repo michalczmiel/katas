1bc7cbf23ee3dbbe69e761be8e2a846d07af7b8c
Michał Czmiel <michalczmiel@outlook.com>
2021-10-02 16:07:46 +0200

    add code for multiple numbers

--- tdd/string-calculator/string_calculator.py
+++ tdd/string-calculator/string_calculator.py
@@ -2,6 +2,4 @@ class StringCalculator:
     def add(self, numbers: str):
         if not numbers:
             return 0
-        if len(numbers) == 3:
-            return int(numbers[0]) + int(numbers[2])
-        return int(numbers[0])
+        return sum(int(number) for number in numbers.split(","))
