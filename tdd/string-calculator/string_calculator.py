87713cbaf42eedf009a8f41025ef7d4c989671fd
Michał Czmiel <michalczmiel@outlook.com>
2021-10-02 16:32:57 +0200

    add code for custom delimiter

--- tdd/string-calculator/string_calculator.py
+++ tdd/string-calculator/string_calculator.py
@@ -4,6 +4,13 @@ class StringCalculator:
     def add(self, numbers: str):
         if not numbers:
             return 0
-        numbers = re.split(",|\n", numbers)
-        numbers = (int(number) for number in numbers)
+        custom_delimiter = None
+        if numbers.startswith("//"):
+            custom_delimiter = numbers.split("\n")[0][2]
+            numbers = numbers[4:]
+        delimiters = ",|\n"
+        if custom_delimiter:
+            delimiters += f"|{custom_delimiter}"
+        numbers = re.split(delimiters, numbers)
+        numbers = [int(number) for number in numbers]
         return sum(numbers)
