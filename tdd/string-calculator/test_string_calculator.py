ecd57410c0102a34d0731343a0d1d29d38605d27
Michał Czmiel <michalczmiel@outlook.com>
2021-10-02 16:00:37 +0200

    add test for single number

--- tdd/string-calculator/test_string_calculator.py
+++ tdd/string-calculator/test_string_calculator.py
@@ -7,6 +7,10 @@ class StringCalculatorTestCase(unittest.TestCase):
     def test_returns_zero_for_empty_string(self):
         self.assertEqual(StringCalculator().add(""), 0)
 
+    def test_returns_number_if_one_number_is_given(self):
+        self.assertEqual(StringCalculator().add("1"), 1)
+
+
 
 if __name__ == "__main__":
     unittest.main()
