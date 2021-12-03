882db24979ede46a869d59764a28ec7f45b412e8
Michał Czmiel <michalczmiel@outlook.com>
2021-12-03 18:08:32 +0100

    add aoc day 1 part 2 solution

--- aoc/2021/day-01/solution.py
+++ aoc/2021/day-01/solution.py
@@ -1,12 +1,14 @@
-from typing import Iterable
+from collections import deque
+from typing import Iterator
 
 
-def read_input() -> Iterable[int]:
+def read_input() -> Iterator[int]:
     with open("input.txt") as file:
-        return [int(line) for line in file.readlines()]
+        for line in file.readlines():
+            yield int(line)
 
 
-def get_measurements_increase_count(measurements: Iterable[int]) -> int:
+def get_measurements_increase_count(measurements: Iterator[int]) -> int:
     previous_measurement = None
     count = 0
 
@@ -19,12 +21,36 @@ def get_measurements_increase_count(measurements: Iterable[int]) -> int:
     return count
 
 
+def get_measurements_sliding_window_sum_increase_count(
+    measurements: Iterator[int],
+) -> int:
+    previous_sliding_window_sum = None
+    sliding_window = deque()
+    count = 0
+
+    for measurement in measurements:
+        sliding_window.append(measurement)
+
+        if len(sliding_window) == 3:
+            sliding_window_sum = sum(sliding_window)
+
+            if (
+                previous_sliding_window_sum
+                and sliding_window_sum > previous_sliding_window_sum
+            ):
+                count += 1
+
+            previous_sliding_window_sum = sliding_window_sum
+            sliding_window.popleft()
+
+    return count
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2021/day/1"""
 
-    measurements = read_input()
-
-    print(get_measurements_increase_count(measurements))
+    print(get_measurements_increase_count(read_input()))
+    print(get_measurements_sliding_window_sum_increase_count(read_input()))
 
 
 if __name__ == "__main__":
