a9bc2cb6f19c90bbe07a70be34a8ae1462b8281f
Michał Czmiel <michalczmiel@outlook.com>
2022-02-19 14:58:24 +0100

    add aoc day 5 part 2

--- aoc/2021/day-05/solution.py
+++ aoc/2021/day-05/solution.py
@@ -44,10 +44,56 @@ def count_overlap_points_horizontal_and_vertical(
     return count
 
 
+def count_overlap_points_horizontal_vertical_diagonal(
+    lines: List[Tuple[int, int, int, int]]
+):
+    overlaps_counter = Counter()
+
+    for x1, y1, x2, y2 in lines:
+        if x1 == x2:
+            y_min = min(y1, y2)
+            y_max = max(y1, y2)
+
+            overlaps_counter.update((x1, y) for y in range(y_min, y_max + 1))
+
+        elif y1 == y2:
+            x_min = min(x1, x2)
+            x_max = max(x1, x2)
+
+            overlaps_counter.update((x, y1) for x in range(x_min, x_max + 1))
+
+        if abs(x1 - x2) == abs(y1 - y2):
+            x_delta = 1 if x1 < x2 else -1
+            y_delta = 1 if y1 < y2 else -1
+
+            points = []
+
+            current_x1 = x1
+            current_y1 = y1
+
+            points.append((current_x1, current_y1))
+            while current_x1 != x2 and current_y1 != y2:
+                current_x1 += x_delta
+                current_y1 += y_delta
+                points.append((current_x1, current_y1))
+
+            overlaps_counter.update(points)
+
+    count = len(
+        [
+            overlap_count
+            for overlap_count in overlaps_counter.values()
+            if overlap_count >= 2
+        ]
+    )
+    return count
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2021/day/5"""
 
     print(count_overlap_points_horizontal_and_vertical(read_input()))
+    print(count_overlap_points_horizontal_vertical_diagonal(read_input()))
 
 
 if __name__ == "__main__":
