7223e7e35ad08636b9ad9c099ea4488fdf293ad2
Michał Czmiel <michalczmiel@outlook.com>
2022-02-20 00:22:47 +0100

    add aoc day 7 part 2

--- aoc/2021/day-07/solution.py
+++ aoc/2021/day-07/solution.py
@@ -6,9 +6,24 @@ def read_input() -> List[int]:
         return [int(value) for value in file.readline().split(",")]
 
 
-def count_required_fuel(positions: List[int]) -> int:
+def get_min_required_fuel(positions: List[int]) -> int:
     costs = {
         sum(abs(position - possible_best_position) for position in positions)
+        for possible_best_position in range(max(positions))
+    }
+
+    return min(costs)
+
+
+def compute_fuel(a, b) -> int:
+    steps = abs(a - b)
+    fuel = round(steps * (steps + 1) / 2)
+    return fuel
+
+
+def get_min_required_fuel_adjusted(positions: List[int]) -> int:
+    costs = {
+        sum(compute_fuel(position, possible_best_position) for position in positions)
         for possible_best_position in range(max(positions))
     }
 
@@ -18,7 +33,8 @@ def count_required_fuel(positions: List[int]) -> int:
 def solution() -> None:
     """Solution to https://adventofcode.com/2021/day/7"""
 
-    print(count_required_fuel(read_input()))
+    print(get_min_required_fuel(read_input()))
+    print(get_min_required_fuel_adjusted(read_input()))
 
 
 if __name__ == "__main__":
