30752b9ebb4e5bd797cc2256efcbc357cbf0aa77
Michał Czmiel <michalczmiel@outlook.com>
2022-09-24 20:52:58 +0200

    handle appending message to a file

--- mocking/file-logger/go/main.go
+++ mocking/file-logger/go/main.go
@@ -10,13 +10,21 @@ type FileLogger struct {
 }
 
 func (l FileLogger) Log(message string) {
-	file, err := os.Create(l.fileName)
+	file, err := os.OpenFile(l.fileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+
+	// file not found
+	if err != nil {
+		file, err = os.Create(l.fileName)
+	}
+
+	defer file.Close()
+
+	// append text to the file
+	_, err = fmt.Fprintln(file, message)
 
 	if err != nil {
 		fmt.Println(err)
 	}
-
-	defer file.Close()
 }
 
 func main() {
