29817e344aec575a2ff550a1ce85e9fca0cc11cc
Michał Czmiel <michalczmiel@outlook.com>
2022-09-25 14:26:36 +0200

    add IsWeekend helper

--- mocking/file-logger/go/clock.go
+++ mocking/file-logger/go/clock.go
@@ -11,3 +11,13 @@ type realClock struct{}
 func (realClock) Now() time.Time {
 	return time.Now()
 }
+
+func IsWeekend(t time.Time) bool {
+	day := t.Weekday()
+
+	if day == time.Saturday || day == time.Sunday {
+		return true
+	}
+
+	return false
+}
