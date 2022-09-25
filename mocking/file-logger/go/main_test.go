6c8dd367862bf58ac6e26acb882f07767afae249
Michał Czmiel <michalczmiel@outlook.com>
2022-09-25 10:32:33 +0200

    extend inital unit test

--- mocking/file-logger/go/main_test.go
+++ mocking/file-logger/go/main_test.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
+type InMemoryFileStorage struct {
+	Logs []string
+}
+
+func (s *InMemoryFileStorage) AppendStringToFile(fileName string, message string) (err error) {
+	s.Logs = append(s.Logs, message)
+
+	return nil
+}
+
 func TestLog(t *testing.T) {
-	storage := LocalFileStorage{}
+	// given
+	storage := &InMemoryFileStorage{}
 	logger := FileLogger{"log.txt", storage}
-	logger.Log("Hello world!")
+
+	// when
+	logger.Log("First log")
+	logger.Log("Second log")
+
+	// then
+	if len(storage.Logs) != 2 {
+		t.Log("Two logs should have been logged but got", len(storage.Logs))
+		t.Fail()
+	}
+
+	expected := []string{"First log", "Second log"}
+
+	if !reflect.DeepEqual(storage.Logs, expected) {
+		t.Log("Logs don't match")
+		t.Fail()
+	}
+
 }
