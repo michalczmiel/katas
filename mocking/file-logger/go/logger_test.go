2103b799026c70ccd3b56d5153494245a604a9af
Michał Czmiel <michalczmiel@outlook.com>
2022-09-25 14:14:46 +0200

    create weekend file if weekend

--- mocking/file-logger/go/logger_test.go
+++ mocking/file-logger/go/logger_test.go
@@ -27,23 +27,25 @@ func (s *InMemoryFileStorage) AppendStringToFile(fileName, message string) error
 	return nil
 }
 
-type fakeClock struct{}
-
-func (fakeClock) Now() time.Time {
-	return time.Date(2022, 9, 24, 10, 0, 0, 0, time.UTC)
+type fakeClock struct {
+	time time.Time
 }
 
-func TestLogWritesToFileWithCurrentDate(t *testing.T) {
+func (fakeClock fakeClock) Now() time.Time {
+	return fakeClock.time
+}
+
+func TestLogWritesToFileWithCurrentDateOnWeekday(t *testing.T) {
 	// given
 	storage := &InMemoryFileStorage{}
-	clock := fakeClock{}
+	clock := fakeClock{time.Date(2022, 9, 22, 10, 0, 0, 0, time.UTC)}
 	logger := FileLogger{storage, clock}
 
 	// when
 	logger.Log("First log")
 	logger.Log("Second log")
 
-	expectedFileName := "log20220924.txt"
+	expectedFileName := "log20220922.txt"
 
 	if storage.Log[expectedFileName] == nil {
 		t.Log("File " + expectedFileName + " not created")
@@ -64,5 +66,70 @@ func TestLogWritesToFileWithCurrentDate(t *testing.T) {
 		t.Log("Logs don't match")
 		t.Fail()
 	}
+}
 
+func TestLogWritesToFileWithCurrentDateOnSaturday(t *testing.T) {
+	// given
+	storage := &InMemoryFileStorage{}
+	clock := fakeClock{time.Date(2022, 9, 24, 10, 0, 0, 0, time.UTC)}
+	logger := FileLogger{storage, clock}
+
+	// when
+	logger.Log("First log")
+	logger.Log("Second log")
+
+	expectedFileName := "weekend.txt"
+
+	if storage.Log[expectedFileName] == nil {
+		t.Log("File " + expectedFileName + " not created")
+		t.Fail()
+	}
+
+	logs := storage.Log[expectedFileName]
+
+	// then
+	if len(logs) != 2 {
+		t.Log("Two logs should have been logged but got", len(storage.Logs))
+		t.Fail()
+	}
+
+	expected := []string{"First log", "Second log"}
+
+	if !reflect.DeepEqual(logs, expected) {
+		t.Log("Logs don't match")
+		t.Fail()
+	}
+}
+
+func TestLogWritesToFileWithCurrentDateOnSunday(t *testing.T) {
+	// given
+	storage := &InMemoryFileStorage{}
+	clock := fakeClock{time.Date(2022, 9, 25, 10, 0, 0, 0, time.UTC)}
+	logger := FileLogger{storage, clock}
+
+	// when
+	logger.Log("First log")
+	logger.Log("Second log")
+
+	expectedFileName := "weekend.txt"
+
+	if storage.Log[expectedFileName] == nil {
+		t.Log("File " + expectedFileName + " not created")
+		t.Fail()
+	}
+
+	logs := storage.Log[expectedFileName]
+
+	// then
+	if len(logs) != 2 {
+		t.Log("Two logs should have been logged but got", len(storage.Logs))
+		t.Fail()
+	}
+
+	expected := []string{"First log", "Second log"}
+
+	if !reflect.DeepEqual(logs, expected) {
+		t.Log("Logs don't match")
+		t.Fail()
+	}
 }
