91fffb2d5925e9c279aefa83f2d5b1846018437b
Michał Czmiel <michalczmiel@outlook.com>
2022-11-01 16:15:24 +0100

    simplify code

--- coding/game-of-life/python/game_of_life.py
+++ coding/game-of-life/python/game_of_life.py
@@ -1,13 +1,19 @@
 import copy
 from typing import List, NewType, Optional
 
+Cell = NewType("Cell", int)
+
 Board = NewType("Board", List[List[int]])
+
+ALIVE = 1
+DEAD = 0
+
+DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1], [-1, -1], [1, 1], [1, -1], [-1, 1]]
 
 
 def get_value(board: Board, x: int, y: int) -> Optional[int]:
     if x < 0 or y < 0:
         return None
-
     try:
         return board[x][y]
     except IndexError:
@@ -16,38 +22,32 @@ def get_value(board: Board, x: int, y: int) -> Optional[int]:
 
 def game_of_life(board: Board) -> Board:
     """
-    Given an input of cells as a board calculate the next state
+    Given an input of cells as a board, calculate the next state
     """
+    if not board:
+        raise Exception("Empty board")
+
     updated_board = copy.deepcopy(board)
 
-    for x, row in enumerate(board):
-        for y, cell in enumerate(row):
-            neighbors = [
-                get_value(board, x + 1, y),
-                get_value(board, x - 1, y),
-                get_value(board, x, y + 1),
-                get_value(board, x, y - 1),
-                get_value(board, x - 1, y - 1),
-                get_value(board, x + 1, y + 1),
-                get_value(board, x + 1, y - 1),
-                get_value(board, x - 1, y + 1),
-            ]
+    m = len(board)
+    n = len(board[0])
 
+    for x in range(m):
+        for y in range(n):
+            neighbors = (get_value(board, x + dx, y + dy) for dx, dy in DIRECTIONS)
             live_neighbors = len([neighbor for neighbor in neighbors if neighbor == 1])
 
             cell = board[x][y]
 
-            if cell == 1:
+            if cell == ALIVE:
                 if live_neighbors < 2:
-                    updated_board[x][y] = 0
+                    updated_board[x][y] = DEAD
                 elif live_neighbors == 2 or live_neighbors == 3:
-                    updated_board[x][y] = 1
+                    updated_board[x][y] = ALIVE
                 else:
-                    updated_board[x][y] = 0
+                    updated_board[x][y] = DEAD
             else:
                 if live_neighbors == 3:
-                    updated_board[x][y] = 1
+                    updated_board[x][y] = ALIVE
 
     return updated_board
-
-
