bc6e39cefac56206da29984e3dd75ff4b01394d9
Michał Czmiel <michalczmiel@outlook.com>
2022-12-02 19:18:57 +0100

    add aoc 2022 day 2 part 2

--- coding/aoc/2022/day-02/solution.py
+++ coding/aoc/2022/day-02/solution.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 ROCK = 1
 PAPER = 2
@@ -7,27 +7,32 @@ SCISSORS = 3
 WIN_SCORE = 6
 DRAW_SCORE = 3
 
+LOOSE = 1
+DRAW = 2
+WIN = 3
+
 OPPONENT_SHAPE_MAPPING = {"A": ROCK, "B": PAPER, "C": SCISSORS}
 PLAYER_SHAPE_MAPPING = {"X": ROCK, "Y": PAPER, "Z": SCISSORS}
+SUGGESTED_MOVE_SHAPE_MAPPING = {"X": LOOSE, "Y": DRAW, "Z": WIN}
 
 
-def read_input(file_name: str) -> List:
-    moves = []
+def read_input(file_name: str) -> List[Tuple[int]]:
+    shapes = []
 
     with open(file_name) as file:
         for line in file:
             opponent, player = line.strip().split(" ")
-            moves.append(
-                (OPPONENT_SHAPE_MAPPING[opponent], PLAYER_SHAPE_MAPPING[player])
-            )
-
-    return moves
+            shapes.append((opponent, player))
+    return shapes
 
 
-def get_total_score_base_on_strategy(moves: List[List[str]]) -> int:
+def get_total_score_based_on_first_strategy(shapes: List[Tuple[str]]) -> int:
     total_score = 0
 
-    for opponent_move, player_move in moves:
+    for opponent_shape, player_shape in shapes:
+        opponent_move = OPPONENT_SHAPE_MAPPING[opponent_shape]
+        player_move = PLAYER_SHAPE_MAPPING[player_shape]
+
         if opponent_move == ROCK:
             if player_move == ROCK:
                 round_score = player_move + DRAW_SCORE
@@ -55,10 +60,45 @@ def get_total_score_base_on_strategy(moves: List[List[str]]) -> int:
     return total_score
 
 
+def get_total_score_based_on_second_strategy(shapes: List[Tuple[str]]) -> int:
+    total_score = 0
+
+    for opponent_shape, player_shape in shapes:
+        opponent_move = OPPONENT_SHAPE_MAPPING[opponent_shape]
+        player_suggested_move = SUGGESTED_MOVE_SHAPE_MAPPING[player_shape]
+
+        if opponent_move == ROCK:
+            if player_suggested_move == LOOSE:
+                round_score = SCISSORS
+            elif player_suggested_move == DRAW:
+                round_score = ROCK + DRAW_SCORE
+            else:
+                round_score = PAPER + WIN_SCORE
+        elif opponent_move == PAPER:
+            if player_suggested_move == LOOSE:
+                round_score = ROCK
+            elif player_suggested_move == DRAW:
+                round_score = PAPER + DRAW_SCORE
+            else:
+                round_score = SCISSORS + WIN_SCORE
+        else:
+            if player_suggested_move == LOOSE:
+                round_score = PAPER
+            elif player_suggested_move == DRAW:
+                round_score = SCISSORS + DRAW_SCORE
+            else:
+                round_score = ROCK + WIN_SCORE
+
+        total_score += round_score
+
+    return total_score
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2022/day/2"""
 
-    print(get_total_score_base_on_strategy(read_input("input.txt")))
+    print(get_total_score_based_on_first_strategy(read_input("input.txt")))
+    print(get_total_score_based_on_second_strategy(read_input("input.txt")))
 
 
 if __name__ == "__main__":
