7da06c6e7205d6617f683650a89feb1c6d768e7e
Michał Czmiel <michalczmiel@outlook.com>
2022-12-02 18:20:55 +0100

    add aoc 2022 day 1 part 2

--- coding/aoc/2022/day-01/solution.py
+++ coding/aoc/2022/day-01/solution.py
@@ -19,6 +19,38 @@ def read_input(file_name: str) -> List:
     return inventories
 
 
+def get_top_carried_calories(inventories: List[List[int]], max_count: int = 3) -> int:
+    max_calories = []
+    to_insert = None
+
+    for inventory in inventories:
+        calories_sum = sum(inventory)
+
+        # skip if sum is smaller than current minimum
+        if max_calories and calories_sum < max_calories[0]:
+            continue
+
+        for index, item in enumerate(max_calories):
+            if calories_sum >= item:
+                continue
+            else:
+                # index for new sum between existing sums
+                to_insert = index - 1
+                break
+
+        if to_insert is not None:
+            max_calories.insert(to_insert, calories_sum)
+            to_insert = None
+        else:
+            max_calories.append(calories_sum)
+
+        # remove lowest value if reached max count
+        if len(max_calories) > max_count:
+            max_calories.pop(0)
+
+    return sum(max_calories)
+
+
 def get_most_carried_calories(inventories: List[List[int]]) -> int:
     max_calories = 0
 
@@ -32,9 +64,10 @@ def get_most_carried_calories(inventories: List[List[int]]) -> int:
 
 
 def solution() -> None:
-    """Solution to https://adventofcode.com/2022/day/2"""
+    """Solution to https://adventofcode.com/2022/day/1"""
 
     print(get_most_carried_calories(read_input("input.txt")))
+    print(get_top_carried_calories(read_input("input.txt")))
 
 
 if __name__ == "__main__":
