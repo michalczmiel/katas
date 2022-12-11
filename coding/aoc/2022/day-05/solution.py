54b015da04e89afac86ac360b5a6b2072e952b93
Michał Czmiel <michalczmiel@outlook.com>
2022-12-11 17:42:37 +0100

    add aco 2022 day 5 part 2

--- coding/aoc/2022/day-05/solution.py
+++ coding/aoc/2022/day-05/solution.py
@@ -53,11 +53,28 @@ def get_top_stack_crates(plan: Plan) -> str:
 
     return "".join(top_crates)
 
+def get_top_stack_crates_with_multiple_crates(plan: Plan) -> str:
+    starting_stacks, instructions = plan
+    stacks = starting_stacks.copy()
+
+    for crates_to_move, from_stack, to_stack in instructions:
+        temporary_stack = []
+        for _ in range(crates_to_move):
+            crate = stacks[from_stack].popleft()
+            temporary_stack.append(crate)
+        for crate in reversed(temporary_stack):
+            stacks[to_stack].appendleft(crate)
+
+    top_crates = [stack.popleft() for _, stack in sorted(stacks.items())]
+
+    return "".join(top_crates)
+
 
 def solution() -> None:
     """Solution to https://adventofcode.com/2022/day/5"""
 
     print(get_top_stack_crates(read_input("input.txt")))
+    print(get_top_stack_crates_with_multiple_crates(read_input("input.txt")))
 
 
 if __name__ == "__main__":
