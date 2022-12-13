1ce48feb9512b0cba0007ffa64cc458c202c145a
Michał Czmiel <michalczmiel@outlook.com>
2022-12-13 21:30:37 +0100

    add aco 2022 day 6 part 1

---
+++ coding/aoc/2022/day-06/solution.py
@@ -0,0 +1,35 @@
+def read_input(file_name: str) -> str:
+    with open(file_name) as file:
+        return file.read()
+
+
+def get_characters_count_before_first_start_of_packet(
+    buffer: str, sequence_length: int = 4
+) -> int:
+    right = 0
+    left = 0
+    unique = set()
+    length = len(buffer)
+
+    while left < length:
+        if buffer[left] not in unique:
+            unique.add(buffer[left])
+        else:
+            unique.clear()
+            right += 1
+            left = right
+
+        if len(unique) == sequence_length:
+            return left + 1
+
+        left += 1
+
+
+def solution() -> None:
+    """Solution to https://adventofcode.com/2022/day/6"""
+
+    print(get_characters_count_before_first_start_of_packet(read_input("input.txt")))
+
+
+if __name__ == "__main__":
+    solution()
