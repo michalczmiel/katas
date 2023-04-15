986bfdc4ac378b7637432716364c334381644838
Michał Czmiel <michalczmiel@outlook.com>
2023-04-15 20:53:40 +0200

    add aoc 2020 day 3 part 2 implementation

--- coding/aoc/2020/day-03/solution.py
+++ coding/aoc/2020/day-03/solution.py
@@ -1,3 +1,7 @@
+import functools
+import operator
+
+
 def read_input() -> list[list[str]]:
     map = []
     with open("input.txt") as file:
@@ -6,21 +10,21 @@ def read_input() -> list[list[str]]:
     return map
 
 
-def count_trees(map: list[list[str]]) -> int:
+def count_trees(map: list[list[str]], right: int, down: int) -> int:
     tree_count = 0
     vertical_max = len(map) - 1
     horizontal = 0
     vertical = 0
 
     while vertical < vertical_max:
-        horizontal += 3
+        horizontal += right
 
         horizontal_max = len(map[vertical])
         # check if out of pattern horizontally
         if horizontal >= horizontal_max:
             horizontal = horizontal - horizontal_max
 
-        vertical += 1
+        vertical += down
 
         if map[vertical][horizontal] == "#":
             tree_count += 1
@@ -28,10 +32,19 @@ def count_trees(map: list[list[str]]) -> int:
     return tree_count
 
 
+def count_trees_multiple_scopes(map: list[list[str]]) -> int:
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    tree_count_list = (count_trees(map, right, down) for right, down in slopes)
+
+    return functools.reduce(operator.mul, tree_count_list)
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2020/day/3"""
 
-    print(count_trees(read_input()))
+    print(count_trees(read_input(), right=3, down=1))
+    print(count_trees_multiple_scopes(read_input()))
 
 
 if __name__ == "__main__":
