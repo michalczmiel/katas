bf2a9cc10d8acb94d0b7c46ab543106b46634a77
Michał Czmiel <michalczmiel@Michas-Mac-mini.local>
2023-04-15 19:46:30 +0200

    add aoc 2020 day 1 part 2 implementation

--- coding/aoc/2020/day-01/solution.py
+++ coding/aoc/2020/day-01/solution.py
@@ -23,10 +23,36 @@ def calculate_part_one(expenses: list[int]) -> int:
     return functools.reduce(operator.mul, entries)
 
 
+def calculate_part_two(expenses: list[int]) -> int:
+    expected_sum = 2020
+    match_count = 2
+    lookup = {}
+    entries = []
+    
+    for expense in expenses:
+        possible_match = lookup.get(expense, [])
+
+        if len(possible_match) == match_count:
+            entries = (*possible_match, expense)
+            break
+        
+        lookup_update = {}
+        for key, value in lookup.items():
+            new_key = key - expense
+            if new_key > 0 and len(value) == match_count - 1:
+                lookup_update[new_key] = [*value, expense]
+        
+        lookup.update(lookup_update)
+        lookup[expected_sum - expense] = [expense]
+
+    return functools.reduce(operator.mul, entries)    
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2020/day/1"""
 
     print(calculate_part_one(read_input()))
+    print(calculate_part_two(read_input()))
 
 
 if __name__ == "__main__":
