001155f64bcd2fea5a962af5374b60bd650be06e
Michał Czmiel <michalczmiel@outlook.com>
2023-06-06 21:46:06 +0200

    add aoc 2020 day 5 part 2 implementation

--- coding/aoc/2020/day-05/solution.py
+++ coding/aoc/2020/day-05/solution.py
@@ -3,6 +3,8 @@ from typing import NewType
 
 
 BoardingPass = NewType("BoardingPass", str)
+SeatRow = NewType("SeatRow", int)
+SeatColumn = NewType("SeatColumn", int)
 SeatId = NewType("SeatId", int)
 
 
@@ -17,9 +19,8 @@ def read_input() -> list[BoardingPass]:
     return boarding_passes
 
 
-def get_seat_id(boarding_pass: BoardingPass) -> SeatId:
+def get_seat_row(boarding_pass: BoardingPass) -> SeatRow:
     rows = boarding_pass[:7]
-    columns = boarding_pass[7:]
 
     left_row = 0
     right_row = 127
@@ -29,6 +30,12 @@ def get_seat_id(boarding_pass: BoardingPass) -> SeatId:
         else:
             left_row = math.ceil((left_row + right_row) / 2)
 
+    return left_row
+
+
+def get_seat_column(boarding_pass: BoardingPass) -> SeatColumn:
+    columns = boarding_pass[-3:]
+
     left_column = 0
     right_column = 7
     for character in columns:
@@ -37,17 +44,31 @@ def get_seat_id(boarding_pass: BoardingPass) -> SeatId:
         else:
             left_column = math.ceil((left_column + right_column) / 2)
 
-    return left_row * 8 + left_column
+    return left_column
+
+
+def get_seat_id(boarding_pass: BoardingPass) -> SeatId:
+    return get_seat_row(boarding_pass) * 8 + get_seat_column(boarding_pass)
 
 
 def get_highest_seat_id(boarding_passes: list[BoardingPass]) -> SeatId:
     return max(get_seat_id(boarding_pass) for boarding_pass in boarding_passes)
 
 
+def get_your_seat_id(boarding_passes: list[BoardingPass]) -> SeatId:
+    seat_ids = sorted(get_seat_id(boarding_pass) for boarding_pass in boarding_passes)
+
+    for index, seat_id in enumerate(seat_ids[1:]):
+        expected_seat_id = seat_ids[index] + 1
+        if seat_id != expected_seat_id:
+            return expected_seat_id
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2020/day/5"""
 
     print(get_highest_seat_id(read_input()))
+    print(get_your_seat_id(read_input()))
 
 
 if __name__ == "__main__":
