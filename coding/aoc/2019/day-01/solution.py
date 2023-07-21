eb48179d60d610e077b2d41df0387c05616606bd
Michał Czmiel <michalczmiel@outlook.com>
2023-07-21 21:00:45 +0200

    add aoc 2019 day 1 part 2 solution

--- coding/aoc/2019/day-01/solution.py
+++ coding/aoc/2019/day-01/solution.py
@@ -2,6 +2,7 @@ from typing import Iterable, NewType
 
 
 Mass = NewType("Mass", int)
+Fuel = NewType("Fuel", int)
 
 
 def read_input() -> Iterable[Mass]:
@@ -10,21 +11,41 @@ def read_input() -> Iterable[Mass]:
             yield int(line)
 
 
-def calculate_sum_of_fuel_requirements(modules_mass: Iterable[Mass]) -> int:
-    fuel_requirement_sum = 0
+def calculate_required_fuel(mass: Mass) -> Fuel:
+    return mass // 3 - 2
+
+
+def calculate_sum_of_fuel_requirements(modules_mass: Iterable[Mass]) -> Fuel:
+    total_fuel = 0
 
     for mass in modules_mass:
-        fuel_requirement = mass // 3 - 2
+        total_fuel += calculate_required_fuel(mass)
 
-        fuel_requirement_sum += fuel_requirement
+    return total_fuel
 
-    return fuel_requirement_sum
+
+def calculate_sum_of_fuel_requirements_adjusted(modules_mass: Iterable[Mass]) -> Fuel:
+    total_fuel = 0
+
+    for mass in modules_mass:
+        initial_fuel = calculate_required_fuel(mass)
+
+        total_fuel += initial_fuel
+
+        additional_fuel = calculate_required_fuel(initial_fuel)
+        while additional_fuel > 0:
+            total_fuel += additional_fuel
+
+            additional_fuel = calculate_required_fuel(additional_fuel)
+
+    return total_fuel
 
 
 def solution() -> None:
     """Solution to https://adventofcode.com/2019/day/1"""
 
     print(calculate_sum_of_fuel_requirements(read_input()))
+    print(calculate_sum_of_fuel_requirements_adjusted(read_input()))
 
 
 if __name__ == "__main__":
