6ca6a36a824935fcffde406b4aa9937ac54f93d1
Michał Czmiel <michalczmiel@outlook.com>
2024-12-01 11:05:22 +0100

    feat: add aoc 2024 day 1 part 2

--- coding/aoc/2024/1.py
+++ coding/aoc/2024/1.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 
 def read_input(file_name: str) -> tuple[list[int], list[int]]:
@@ -28,10 +29,28 @@ def calculate_total_distance_between_lists(lists: tuple[list[int], list[int]]) -
     return total_distance
 
 
+def calculate_total_similarity_score(lists: tuple[list[int], list[int]]) -> int:
+    first, second = lists
+
+    total_similarity_score = 0
+
+    first_counter = collections.Counter(first)
+    second_counter = collections.Counter(second)
+
+    for number, count in first_counter.items():
+        right_count = second_counter.get(number)
+
+        if right_count is not None:
+            total_similarity_score += number * count * right_count
+
+    return total_similarity_score
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2024/day/1"""
 
     print(calculate_total_distance_between_lists(read_input("1.txt")))
+    print(calculate_total_similarity_score(read_input("1.txt")))
 
 
 if __name__ == "__main__":
