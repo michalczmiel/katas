b14c22109d2563103daf4a01ab58e641aaf26bba
Michał Czmiel <michalczmiel@outlook.com>
2024-12-07 12:50:38 +0100

    add aoc 2024 day 6 part 1

---
+++ coding/aoc/2024/6.py
@@ -0,0 +1,81 @@
+from typing import NewType
+
+EMPTY = 0
+OBSTRUCTION = 1
+GUARD = 2
+
+LabMap = NewType("LabMap", dict[tuple[int, int], int])
+
+
+def read_input(file_name: str) -> LabMap:
+    lab_map = {}
+    element_mapping = {
+        ".": EMPTY,
+        "#": OBSTRUCTION,
+        "^": GUARD,
+    }
+
+    for y, line in enumerate(open(file_name)):
+        for x, char in enumerate(line.strip()):
+            lab_map[(x, y)] = element_mapping[char]
+
+    return lab_map
+
+
+def find_start_position(lab_map: LabMap) -> tuple[int, int]:
+    for (x, y), element in lab_map.items():
+        if element == GUARD:
+            return (x, y)
+
+
+def get_next_position(
+    current: tuple[int, int], direction: tuple[int, int]
+) -> tuple[int, int]:
+    return (
+        current[0] + direction[0],
+        current[1] + direction[1],
+    )
+
+
+def rotate_direction(direction: tuple[int, int]) -> tuple[int, int]:
+    direction_rotation_mapping = {
+        (0, -1): (1, 0),
+        (1, 0): (0, 1),
+        (0, 1): (-1, 0),
+        (-1, 0): (0, -1),
+    }
+
+    return direction_rotation_mapping[direction]
+
+
+def count_visited_positions(lab_map: LabMap) -> int:
+    direction = (0, -1)
+    visited = set()
+    current = find_start_position(lab_map)
+
+    while True:
+        visited.add(current)
+
+        next_position = get_next_position(current, direction)
+
+        next_element = lab_map.get(next_position)
+        if next_element is None:
+            return len(visited)
+
+        while next_element == OBSTRUCTION:
+            direction = rotate_direction(direction)
+            next_position = get_next_position(current, direction)
+            next_element = lab_map.get(next_position)
+
+        current = next_position
+
+
+def solution() -> None:
+    """Solution to https://adventofcode.com/2024/day/6"""
+
+    lab_map = read_input("6.txt")
+    print(count_visited_positions(lab_map))
+
+
+if __name__ == "__main__":
+    solution()
