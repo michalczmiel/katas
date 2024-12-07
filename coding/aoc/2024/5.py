61f79c1230b60bac25fa868154f72a0d1c6915da
Michał Czmiel <michalczmiel@outlook.com>
2024-12-07 10:51:25 +0100

    add aoc 2024 day 5 part 2

--- coding/aoc/2024/5.py
+++ coding/aoc/2024/5.py
@@ -1,7 +1,11 @@
 from collections import defaultdict
+from typing import NewType
+
+Rules = NewType("Rules", list[tuple[int, int]])
+Update = NewType("Update", list[int])
 
 
-def read_input(file_name: str) -> tuple[list[tuple[int, int]], list[list[int]]]:
+def read_input(file_name: str) -> tuple[Rules, list[Update]]:
     rules = []
     updates = []
 
@@ -17,22 +21,20 @@ def read_input(file_name: str) -> tuple[list[tuple[int, int]], list[list[int]]]:
     return rules, updates
 
 
-def is_correct_update(page_ordering: dict[int, set], update: list[int]) -> bool:
+def is_correct_update(page_ordering: dict[int, set], update: Update) -> bool:
     for i in range(1, len(update)):
+        number = update[i]
+        numbers_after = page_ordering.get(number, set())
+
         for j in range(0, i):
             previous_number = update[j]
-            number = update[i]
 
-            numbers_after = page_ordering.get(number, set())
             if previous_number in numbers_after:
                 return False
-
     return True
 
 
-def sum_middle_page_from_correct_updates(
-    rules: list[tuple[int, int]], updates: list[list[int]]
-) -> int:
+def sum_middle_page_from_correct_updates(rules: Rules, updates: list[Update]) -> int:
     page_ordering = defaultdict(set)
     for first, second in rules:
         page_ordering[first].add(second)
@@ -46,12 +48,49 @@ def sum_middle_page_from_correct_updates(
     return total_sum
 
 
+def sort_update(page_ordering: dict[int, set], update: Update) -> Update:
+    """
+    Current time complexity is O(n^2), could be improved
+    """
+    ordered = update.copy()
+
+    for i in range(1, len(ordered)):
+        current = ordered[i]
+        numbers_after = page_ordering.get(current, set())
+
+        for j in range(0, i):
+            previous = ordered[j]
+
+            if previous in numbers_after:
+                ordered.insert(j, current)
+                del ordered[i + 1]
+                break
+
+    return ordered
+
+
+def sum_middle_page_from_incorrect_updates(rules: Rules, updates: list[Update]) -> int:
+    page_ordering = defaultdict(set)
+    for first, second in rules:
+        page_ordering[first].add(second)
+
+    total_sum = 0
+
+    for update in updates:
+        if not is_correct_update(page_ordering, update):
+            ordered_update = sort_update(page_ordering, update)
+            total_sum += ordered_update[len(ordered_update) // 2]
+
+    return total_sum
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2024/day/5"""
 
     rules, updates = read_input("5.txt")
 
     print(sum_middle_page_from_correct_updates(rules, updates))
+    print(sum_middle_page_from_incorrect_updates(rules, updates))
 
 
 if __name__ == "__main__":
