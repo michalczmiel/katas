7a1b7723875153bc27b2af068785ae7cebc94d42
Michał Czmiel <michalczmiel@outlook.com>
2024-12-08 16:19:51 +0100

    add aoc 2024 day 8 part 2

--- coding/aoc/2024/8.py
+++ coding/aoc/2024/8.py
@@ -66,11 +66,66 @@ def count_unique_antinodes(antenna_map: AntennaMap) -> int:
     return len(unique_antinodes)
 
 
+def create_antinode(
+    a: tuple[int, int], b: tuple[int, int], r: int = 1
+) -> tuple[int, int]:
+    x1, y1 = a
+    x2, y2 = b
+
+    v1 = (x1 - x2, y1 - y2)
+
+    return (x1 + v1[0] * r, y1 + v1[1] * r)
+
+
+def find_antinodes_with_resonant_harmonics(
+    locations: tuple[int, int], max_x: int, max_y: int
+) -> set[tuple[int, int]]:
+    antinodes = set()
+
+    location_pairs = combinations(locations, 2)
+
+    for a, b in location_pairs:
+        antinodes.add(a)
+        antinodes.add(b)
+
+        iteration = 1
+
+        a1 = create_antinode(a, b, iteration)
+        while not is_out_of_bounds(max_x, max_y, a1):
+            antinodes.add(a1)
+            iteration += 1
+            a1 = create_antinode(a, b, iteration)
+
+        iteration = 1
+
+        a2 = create_antinode(b, a, iteration)
+        while not is_out_of_bounds(max_x, max_y, a2):
+            antinodes.add(a2)
+            iteration += 1
+            a2 = create_antinode(b, a, iteration)
+
+    return antinodes
+
+
+def count_unique_antinodes_with_resonant_harmonics(antenna_map: AntennaMap) -> int:
+    unique_antinodes = set()
+
+    for _, locations in antenna_map.locations_per_antenna.items():
+        unique_antinodes.update(
+            find_antinodes_with_resonant_harmonics(
+                locations, antenna_map.max_x, antenna_map.max_y
+            )
+        )
+
+    return len(unique_antinodes)
+
+
 def solution() -> None:
     """Solution to https://adventofcode.com/2024/day/8"""
 
     data = read_input("8.txt")
     print(count_unique_antinodes(data))
+    print(count_unique_antinodes_with_resonant_harmonics(data))
 
 
 if __name__ == "__main__":
