b366050b6285d841b64d6973f26d30ba9ac59d59
Michał Czmiel <michalczmiel@outlook.com>
2024-12-08 16:26:18 +0100

    cleanup aoc 2024 day 8

--- coding/aoc/2024/8.py
+++ coding/aoc/2024/8.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from dataclasses import dataclass
-from itertools import combinations
+from itertools import combinations, permutations
+from typing import Callable
 
 
 @dataclass
@@ -32,6 +33,18 @@ def is_out_of_bounds(max_x: int, max_y: int, point: tuple[int, int]) -> bool:
     return x > max_x or y > max_y or x < 0 or y < 0
 
 
+def create_antinode(
+    a: tuple[int, int], b: tuple[int, int], r: int = 1
+) -> tuple[int, int]:
+    x1, y1 = a
+    x2, y2 = b
+
+    dx = x1 - x2
+    dy = y1 - y2
+
+    return (x1 + dx * r, y1 + dy * r)
+
+
 def find_antinodes(
     locations: tuple[int, int], max_x: int, max_y: int
 ) -> set[tuple[int, int]]:
@@ -39,42 +52,14 @@ def find_antinodes(
 
     location_pairs = combinations(locations, 2)
 
-    for (x1, y1), (x2, y2) in location_pairs:
-        v1 = (x1 - x2, y1 - y2)
-        a1 = (x1 + v1[0], y1 + v1[1])
-
-        if not is_out_of_bounds(max_x, max_y, a1):
-            antinodes.add(a1)
-
-        v2 = (x2 - x1, y2 - y1)
-        a2 = (x2 + v2[0], y2 + v2[1])
-
-        if not is_out_of_bounds(max_x, max_y, a2):
-            antinodes.add(a2)
+    for pair in location_pairs:
+        # checking antinodes above and bellow antennas
+        for a, b in permutations(pair):
+            antinode = create_antinode(a, b)
+            if not is_out_of_bounds(max_x, max_y, antinode):
+                antinodes.add(antinode)
 
     return antinodes
-
-
-def count_unique_antinodes(antenna_map: AntennaMap) -> int:
-    unique_antinodes = set()
-
-    for _, locations in antenna_map.locations_per_antenna.items():
-        unique_antinodes.update(
-            find_antinodes(locations, antenna_map.max_x, antenna_map.max_y)
-        )
-
-    return len(unique_antinodes)
-
-
-def create_antinode(
-    a: tuple[int, int], b: tuple[int, int], r: int = 1
-) -> tuple[int, int]:
-    x1, y1 = a
-    x2, y2 = b
-
-    v1 = (x1 - x2, y1 - y2)
-
-    return (x1 + v1[0] * r, y1 + v1[1] * r)
 
 
 def find_antinodes_with_resonant_harmonics(
@@ -84,37 +69,30 @@ def find_antinodes_with_resonant_harmonics(
 
     location_pairs = combinations(locations, 2)
 
-    for a, b in location_pairs:
-        antinodes.add(a)
-        antinodes.add(b)
+    for pair in location_pairs:
+        # antennas positions are also antinodes
+        antinodes.add(pair[0])
+        antinodes.add(pair[1])
 
-        iteration = 1
+        # checking antinodes above and bellow antennas
+        for a, b in permutations(pair):
+            iteration = 1
 
-        a1 = create_antinode(a, b, iteration)
-        while not is_out_of_bounds(max_x, max_y, a1):
-            antinodes.add(a1)
-            iteration += 1
-            a1 = create_antinode(a, b, iteration)
-
-        iteration = 1
-
-        a2 = create_antinode(b, a, iteration)
-        while not is_out_of_bounds(max_x, max_y, a2):
-            antinodes.add(a2)
-            iteration += 1
-            a2 = create_antinode(b, a, iteration)
+            antinode = create_antinode(a, b, iteration)
+            while not is_out_of_bounds(max_x, max_y, antinode):
+                antinodes.add(antinode)
+                iteration += 1
+                antinode = create_antinode(a, b, iteration)
 
     return antinodes
 
 
-def count_unique_antinodes_with_resonant_harmonics(antenna_map: AntennaMap) -> int:
+def count_unique_antinodes(antenna_map: AntennaMap, finder_function: Callable) -> int:
     unique_antinodes = set()
 
     for _, locations in antenna_map.locations_per_antenna.items():
         unique_antinodes.update(
-            find_antinodes_with_resonant_harmonics(
-                locations, antenna_map.max_x, antenna_map.max_y
-            )
+            finder_function(locations, antenna_map.max_x, antenna_map.max_y)
         )
 
     return len(unique_antinodes)
@@ -124,8 +102,12 @@ def solution() -> None:
     """Solution to https://adventofcode.com/2024/day/8"""
 
     data = read_input("8.txt")
-    print(count_unique_antinodes(data))
-    print(count_unique_antinodes_with_resonant_harmonics(data))
+    print(count_unique_antinodes(data, finder_function=find_antinodes))
+    print(
+        count_unique_antinodes(
+            data, finder_function=find_antinodes_with_resonant_harmonics
+        )
+    )
 
 
 if __name__ == "__main__":
