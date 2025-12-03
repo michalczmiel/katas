41f7e38ea22629bdf9dbae67198c5d2ebd966f97
Michał Czmiel <michalczmiel@outlook.com>
2025-12-03 20:40:58 +0100

    feat: add aoc 2025 day 2 part 2

--- coding/aoc/2025/02.py
+++ coding/aoc/2025/02.py
@@ -20,7 +20,6 @@ def is_invalid_id(id: int) -> bool:
     >>> is_invalid_id(123123)
     True
     """
-
     str_id = str(id)
 
     if len(str_id) % 2 == 1:
@@ -31,12 +30,37 @@ def is_invalid_id(id: int) -> bool:
     return str_id[0:middle] == str_id[middle:]
 
 
+def is_invalid_id_improved(id: int) -> bool:
+    """
+    >>> is_invalid_id_improved(999)
+    True
+    >>> is_invalid_id_improved(565656)
+    True
+    >>> is_invalid_id_improved(2121212118)
+    False
+    """
+    id_str = str(id)
+
+    n = len(id_str)
+
+    for i in range(1, n):
+        tmp = id_str[:i]
+
+        left = id_str[i:]
+
+        # check if all the repetitions match the full length
+        if (left.count(tmp) + 1) * len(tmp) == n:
+            return True
+
+    return False
+
+
 def get_invalid_ids_from_range(ids_range: tuple[int, int]) -> list[int]:
     first, last = ids_range
 
     invalid_ids = []
     for i in range(first, last + 1):
-        if is_invalid_id(i):
+        if is_invalid_id_improved(i):
             invalid_ids.append(i)
 
     return invalid_ids
