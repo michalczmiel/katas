a3d265bfb82ed30da690ef456fa571f643e09b60
Michał Czmiel <michalczmiel@outlook.com>
2025-12-03 10:21:32 +0100

    feat: add aoc 2025 day 2 part 1

---
+++ coding/aoc/2025/02.py
@@ -0,0 +1,59 @@
+def read_input(file_name: str) -> list[tuple[int, int]]:
+    with open(file_name) as file:
+        ranges = file.read().strip().split(",")
+
+        result = []
+        for id_range in ranges:
+            first, second = id_range.split("-")
+            result.append((int(first), int(second)))
+        return result
+
+
+def is_invalid_id(id: int) -> bool:
+    """
+    >>> is_invalid_id(1)
+    False
+    >>> is_invalid_id(123)
+    False
+    >>> is_invalid_id(11)
+    True
+    >>> is_invalid_id(123123)
+    True
+    """
+
+    str_id = str(id)
+
+    if len(str_id) % 2 == 1:
+        return False
+
+    middle = len(str_id) // 2
+
+    return str_id[0:middle] == str_id[middle:]
+
+
+def get_invalid_ids_from_range(ids_range: tuple[int, int]) -> list[int]:
+    first, last = ids_range
+
+    invalid_ids = []
+    for i in range(first, last + 1):
+        if is_invalid_id(i):
+            invalid_ids.append(i)
+
+    return invalid_ids
+
+
+def sum_invalid_ids(ranges: list[tuple[int, int]]) -> int:
+    total = 0
+    for r in ranges:
+        total += sum(get_invalid_ids_from_range(r))
+    return total
+
+
+def solution() -> None:
+    """Solution to https://adventofcode.com/2025/day/2"""
+
+    print(sum_invalid_ids(read_input("02.txt")))
+
+
+if __name__ == "__main__":
+    solution()
